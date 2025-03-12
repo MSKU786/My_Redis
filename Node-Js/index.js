a8ccf565457d02fc27ae998486f777ce3fdc0409
MSKU786 <manishsingh332000@gmail.com>
2025-03-12 11:28:11 +0530

    Added redis parser library to handle redis commands

--- Node-Js/index.js
+++ Node-Js/index.js
@@ -1,7 +1,20 @@
 const net = require('net');
+const Parser = require('redis-parser');
 
 const server = net.createServer((connection) => {
   console.log('Client Connected');
+  connection.on('data', (data) => {
+    const parser = new Parser({
+      returnReply: (reply) => {
+        console.log('=>', reply);
+      },
+      returnError: (err) => {
+        console.log('=>', err);
+      },
+    });
+    parser.execute(data);
+    console.log('->', data.toString());
+  });
 });
 
 server.listen(8000, () => {
