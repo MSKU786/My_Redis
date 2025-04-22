a7f5f57ff03e12b3ca6f7a4504d454eadf22a6b0
MSKU786 <manishsingh332000@gmail.com>
2025-04-22 11:52:25 +0530

    Added necessary comment to understnad the flow

--- GoLang/main.go
+++ GoLang/main.go
@@ -1,31 +1,37 @@
 package main
 
 import (
-	"bufio"
+	"bufio" // Helps to read data from the connection with buffering (like readline in Node).
 	"fmt"
-	"net"
+	"net" //Go's built-in networking package to work with TCP/UDP.
 )
 
 func main () {
 	// Start listening on a port 
 	listener, err := net.Listen("tcp", ":8000");
+	// : Opens a TCP server on port 8080 (on all interfaces)
+
+	// Error handling if the port is in use or invalid.
 	if err != nil {
 		fmt.Println("Error starting tcp server", err);
 		return;
 	}
 
+	// Ensures the listener is closed when the program ends.
 	defer listener.Close();
 
 	fmt.Println("TCP server is listening on port: 8000");
 
 	for {
-		//Aceept a connection
+		//Accepts new clients one by one in a loop like socket in node.js
+		// conn is a net.Conn object
 		conn, err := listener.Accept();
 		if err != nil {
 			fmt.Println("Error accepting connection:", err)
 			continue
 		}
 		// Handle the connection in a new goroutine
+		// Spawns a goroutine to handle each client concurrently
 		go handleConnection(conn)
 	}
 
@@ -35,13 +41,16 @@ func handleConnection(conn net.Conn) {
 	defer conn.Close();
 
 	fmt.Println("Client Connected...", conn.RemoteAddr());
+	
+	//Creates a line-by-line reader.
 	scanner := bufio.NewScanner(conn);
 
 	for scanner.Scan() {
+
 		text := scanner.Text();
 		fmt.Println("Recieved", text);
 
-		//Echo back the message
+		//Echo back the message Sends back a message to the client.
 		_,err := conn.Write([]byte("Echo:" + text + "\n"));
 		if (err != nil) {
 			fmt.Println("Error writting to client", err);
