d615d62ae322db902779551f08d5bad909ef8f09
MSKU786 <manishsingh332000@gmail.com>
2025-04-29 10:34:38 +0530

    Added map stoer and get command

--- GoLang/utils/commands.go
+++ GoLang/utils/commands.go
@@ -1,16 +1,50 @@
-package utils 
+package utils
+
+// Adjusted to proper Go module path
+
+import (
+	"sync"
+	"time"
+)
+
+var (
+	Store       = sync.Map{}
+	ExpireStore = sync.Map{}
+) // Assuming NewStore initializes the store
 
 // Define a function type of commands
 type CommandFunc func(args []string) interface{}
 
 var commands = map[string]CommandFunc {
 	"SET": func(args []string) interface{} {
-			if (len(args) < 2) {
-					return "ERR wrong number of arguments for 'set'"
+			if len(args) < 2 {
+				return "Error: SET command requires at least 2 arguments"
 			}
 
-			key, value = args[0],args[1];
-			store.Store.store(key, value);
-			
-	}
+			key, value := args[0], args[1]
+			Store.Store(key, value)
+			return "OK"
+	},
+	"GET": func(args []string) interface{} {
+			if len(args) < 1 {
+				return "Error: GET command requires at least 1 argument"
+			}
+	
+			key := args[0]
+			value, exists := Store.Load(key) // Using Store.Load to retrieve the value and check existence
+			ttl, ttlExists := ExpireStore.Load(key) // Using ExpireStore.Load to retrieve TTL and check existence
+	
+			if ttlExists {
+				if ttl.(int64) < time.Now().Unix() { // Assuming TTL is stored as int64 (Unix timestamp)
+					Store.Delete(key) // Assuming Store.Delete removes the key
+					ExpireStore.Delete(key) // Assuming ExpireStore.Delete removes the TTL
+					return nil
+				}
+			}
+	
+			if exists {
+				return value
+			}
+			return nil
+		},
 }
--- GoLang/utils/map.go
+++
@@ -1,8 +0,0 @@
-package utils
-
-import "sync"
-
-var (
-	Store       = sync.Map{}
-	ExpireStore = sync.Map{}
-)
