2eb5d0787f6f3e29238393c467e68b641ac7b645
MSKU786 <manishsingh332000@gmail.com>
2025-04-30 11:09:56 +0530

    Resolved syntax bugs

--- GoLang/utils/commands.go
+++ GoLang/utils/commands.go
@@ -16,10 +16,10 @@ var (
 // Define a function type of commands
 type CommandFunc func(args []string) interface{}
 
-var commands = map[string]CommandFunc {
+var commands = map[string]CommandFunc{
 	"SET": func(args []string) interface{} {
 			if len(args) < 2 {
-				return "Error: SET command requires at least 2 arguments"
+					return "Error: SET command requires at least 2 arguments"
 			}
 
 			key, value := args[0], args[1]
@@ -28,53 +28,60 @@ var commands = map[string]CommandFunc {
 	},
 	"GET": func(args []string) interface{} {
 			if len(args) < 1 {
-				return "Error: GET command requires at least 1 argument"
+					return "Error: GET command requires 1 argument"
 			}
-	
+
 			key := args[0]
-			value, exists := Store.Load(key) // Using Store.Load to retrieve the value and check existence
-			ttl, ttlExists := ExpireStore.Load(key) // Using ExpireStore.Load to retrieve TTL and check existence
-	
+			value, exists := Store.Load(key)
+			ttl, ttlExists := ExpireStore.Load(key)
+
 			if ttlExists {
-				if ttl.(int64) < time.Now().Unix() { // Assuming TTL is stored as int64 (Unix timestamp)
-					Store.Delete(key) // Assuming Store.Delete removes the key
-					ExpireStore.Delete(key) // Assuming ExpireStore.Delete removes the TTL
-					return nil
-				}
+					expirationTime, ok := ttl.(int64)
+					if !ok {
+							return "Error: Invalid TTL format"
+					}
+					if expirationTime < time.Now().Unix() {
+							Store.Delete(key)
+							ExpireStore.Delete(key)
+							return nil
+					}
 			}
-	
+
 			if exists {
-				return value
+					return value
 			}
 			return nil
-		},
-		"EXPIRE": func(args []string) interface{} { 
-				key, seconds := args[0], args[1]
-
-				ttl , err := strconv.Atoi(seconds)
-				if (err != nil) {
-					return nil
-				}
-				ttl = ttl * 1000;
-
-				value, ok := Store[key];
-				if (ok) {
-					var currentTime = time.Now().Unix();
-					ExpireStore[key] =  currentTime + ttl;
-					return 1;
-				}
-				return 0;
-		},
-		"DEL": (args) => {
-			var count = 0
-			for i : range(args) {
-				_, ok := Store[i];
-				if (ok) {
-					Store.Delete(i);
-					ExpireStore.Delete(i);
-					count += 1
-				}
+	},
+	"EXPIRE": func(args []string) interface{} {
+			if len(args) < 2 {
+					return "Error: EXPIRE command requires 2 arguments"
 			}
-			return count;
-		}
+
+			key, seconds := args[0], args[1]
+			ttl, err := strconv.Atoi(seconds)
+			if err != nil {
+					return "Error: Invalid TTL value"
+			}
+
+			_, ok := Store.Load(key)
+			if ok {
+					currentTime := time.Now().Unix()
+					expirationTime := currentTime + int64(ttl)
+					ExpireStore.Store(key, expirationTime)
+					return 1
+			}
+			return 0
+	},
+	"DEL": func(args []string) interface{} {
+			var count = 0
+			for _, key := range args {
+					_, ok := Store.Load(key)
+					if ok {
+							Store.Delete(key)
+							ExpireStore.Delete(key)
+							count++
+					}
+			}
+			return count
+	},
 }
